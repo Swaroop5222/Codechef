abd17245d26f5c180216cd0e49d7d80f4016241a
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-07 21:18:46 +0530

    Create Squats.c

---
+++ Squats.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x;
+	    scanf("%d",&x);
+	    printf("%d\n",15*x);
+	}
+	return 0;
+}
+
