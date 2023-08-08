300773e177093b5b0678f5a640a06ff2725f9632
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-08 21:57:13 +0530

    Create Ludo.c

---
+++ Ludo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	    int x;
+	    scanf("%d",&x);
+	    if(x==6){
+	        printf("yes\n");
+	        
+	    }
+	    else{
+	        printf("no\n");
+	    }
+	        
+	    
+	        
+	    
+	}
+	return 0;
+}
+
