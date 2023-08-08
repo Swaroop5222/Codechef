ec4c533c9e695626034bd9d51fcba85f8603f238
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-08 21:53:26 +0530

    Create Puzzle hunt.c

---
+++ Puzzle hunt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int n;
+	scanf("%d ",&n);
+	  if(n==6 || n==7 || n==8){
+	        printf("yes\n");
+	    }
+	    else{
+	        printf("no\n");
+	    }
+	return 0;
+}
+
