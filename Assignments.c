b005eb75ef799d893c2aebfcdc1fa6ee6121734a
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-09 21:46:59 +0530

    Create Assignments.c

---
+++ Assignments.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t, x;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&x);
+	    if(x<=7)
+	    printf("yes\n");
+	    else
+	    printf("no\n");
+	}
+	return 0;
+}
+
