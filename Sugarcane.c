d08d91d8cacc5228b2c1b8cf6a01cfca42250304
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-10 15:33:48 +0530

    Create Sugarcane.c

---
+++ Sugarcane.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t, n;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&n);
+	    printf("%.f\n",0.3*50*n);
+	}
+	return 0;
+}
+
