fe3e1de5afeea7c048f28ba70c6915c842ae20b6
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-10 15:25:40 +0530

    Create Notebooks.c

---
+++ Notebooks.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes he
+	int t, n;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&n);
+	    printf("%d\n",n*1000/100);
+	}
+	return 0;
+}
+
