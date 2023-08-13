107933a07e96ddcdb7594985fa3aef1084982f03
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-13 15:34:34 +0530

    Create Police and thief.c

---
+++ Police and thief.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t, x, y;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d%d",&x,&y);
+	    if(x>y)
+	    printf("%d\n",x-y);
+	    else
+	    printf("%d\n",y-x);
+	}
+	return 0;
+}
+
