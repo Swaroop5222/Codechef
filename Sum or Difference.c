91f165f49ed5e6feb4b4d7b7a911687e73fb601e
Swaroop <141646466+Swaroop5222@users.noreply.github.com>
2023-08-18 12:57:47 +0530

    Create Sum or Difference.c

---
+++ Sum or Difference.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n1, n2;
+	scanf("%d%d",&n1,&n2);
+	if(n1>n2)
+	printf("%d\n",(n1-n2));
+	else
+	printf("%d\n",n1+n2);
+	return 0;
+}
+
