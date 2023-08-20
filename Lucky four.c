cc5a9c6cccdd119e5029d3f9c46e4b21b9e7e266
Swaroop <141646466+Swaroop5222@users.noreply.github.com>
2023-08-20 10:45:45 +0530

    Create Lucky four.c

---
+++ Lucky four.c
@@ -0,0 +1,34 @@
+//We have populated the solutions for the 10 easiest problems for your support.
+//Click on the SUBMIT button to make a submission to this problem.
+
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	
+	int t;
+	scanf("%d",&t);
+	
+	while(t--)
+	{
+	      int n;
+	    scanf("%d",&n);
+	    
+	    int cnt=0;
+	    while(n)
+	    {
+	        int m=(n%10);
+	        
+	        if(m==4) cnt++;
+	        
+	        n/=10;
+	    }
+	    printf("%d\n",cnt);
+	}
+	  
+	
+	return 0;
+}
+
+
+
